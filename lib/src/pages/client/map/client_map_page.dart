7b4f17e8568a3acadef6376fd656ac17b4680f2f
guslublin <guslublin@gmail.com>
2022-02-17 15:42:22 -0300

    Diseño de pantalla del viaje

--- lib/src/pages/client/map/client_map_page.dart
+++ lib/src/pages/client/map/client_map_page.dart
@@ -32,7 +32,7 @@ class _CLientMapPageState extends State<CLientMapPage> {
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       key: _con.key,
       drawer: _drawer(),
@@ -60,18 +60,20 @@ class _CLientMapPageState extends State<CLientMapPage> {
     );
   }
 
-  Widget _buttonDrawer(){
+  Widget _buttonDrawer() {
     return Container(
       alignment: Alignment.centerLeft,
       child: IconButton(
         onPressed: _con.openDrawer,
-        icon: Icon(Icons.menu, color: Colors.white,),
+        icon: Icon(
+          Icons.menu,
+          color: Colors.white,
+        ),
       ),
     );
   }
 
-
-  Widget _buttonRequest(){
+  Widget _buttonRequest() {
     return Container(
       height: 50,
       alignment: Alignment.centerLeft,
@@ -85,8 +87,12 @@ class _CLientMapPageState extends State<CLientMapPage> {
     );
   }
 
-  Widget _iconMyLocation(){
-    return Image.asset('assets/img/my_location.png', width: 65, height: 65,);
+  Widget _iconMyLocation() {
+    return Image.asset(
+      'assets/img/my_location.png',
+      width: 65,
+      height: 65,
+    );
   }
 
   Widget _drawer() {
@@ -104,8 +110,7 @@ class _CLientMapPageState extends State<CLientMapPage> {
                     style: TextStyle(
                         fontSize: 18,
                         color: Colors.black,
-                        fontWeight: FontWeight.bold
-                    ),
+                        fontWeight: FontWeight.bold),
                     maxLines: 1,
                   ),
                 ),
@@ -115,27 +120,26 @@ class _CLientMapPageState extends State<CLientMapPage> {
                     style: TextStyle(
                         fontSize: 14,
                         color: Colors.grey[800],
-                        fontWeight: FontWeight.bold
-                    ),
+                        fontWeight: FontWeight.bold),
                     maxLines: 1,
                   ),
                 ),
-                SizedBox(height: 10,),
+                SizedBox(
+                  height: 10,
+                ),
                 CircleAvatar(
                   backgroundImage: AssetImage('assets/img/profile_2.jpg'),
                   radius: 40,
                 )
               ],
             ),
-            decoration: BoxDecoration(
-                color: Colors.amber
-            ),
+            decoration: BoxDecoration(color: Colors.amber),
           ),
           ListTile(
             title: Text('Editar Perfil'),
             trailing: Icon(Icons.edit),
             //leading: Icon(Icons.cancel),
-            onTap: (){},
+            onTap: () {},
           ),
           ListTile(
             title: Text('Cerrar Sesión'),
@@ -148,7 +152,7 @@ class _CLientMapPageState extends State<CLientMapPage> {
     );
   }
 
-  Widget _buttonCenterPosition(){
+  Widget _buttonCenterPosition() {
     return GestureDetector(
       onTap: _con.centerPosition,
       child: Container(
@@ -160,16 +164,17 @@ class _CLientMapPageState extends State<CLientMapPage> {
           shape: CircleBorder(),
           child: Container(
               padding: EdgeInsets.all(10),
-              child: Icon(Icons.location_searching,
+              child: Icon(
+                Icons.location_searching,
                 color: Colors.grey[600],
-                size: 20,)
-          ),
+                size: 20,
+              )),
         ),
       ),
     );
   }
 
-  Widget _buttonChangeTo(){
+  Widget _buttonChangeTo() {
     return GestureDetector(
       onTap: _con.changeFromTo,
       child: Container(
@@ -184,15 +189,14 @@ class _CLientMapPageState extends State<CLientMapPage> {
               child: Icon(
                 Icons.refresh,
                 color: Colors.grey[600],
-                size: 20,)
-          ),
+                size: 20,
+              )),
         ),
       ),
     );
   }
 
-  Widget _googleMapsWidget(){
-
+  Widget _googleMapsWidget() {
     return GoogleMap(
       mapType: MapType.normal,
       initialCameraPosition: _con.initialPosition,
@@ -200,7 +204,7 @@ class _CLientMapPageState extends State<CLientMapPage> {
       myLocationEnabled: false,
       myLocationButtonEnabled: false,
       markers: Set<Marker>.of(_con.markers.values),
-      onCameraMove: (position){
+      onCameraMove: (position) {
         _con.initialPosition = position;
       },
       onCameraIdle: () async {
@@ -209,41 +213,37 @@ class _CLientMapPageState extends State<CLientMapPage> {
     );
   }
 
-  Widget _cardGooglePlace(){
+  Widget _cardGooglePlace() {
     return Container(
       width: MediaQuery.of(context).size.width * 0.9,
       child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20)
-        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
         child: Container(
           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              _infoCardLocation(
-                'Desde',
-                _con.from ?? 'Lugar de recogida',
-                () async {
-                  _con.showGoogleAutocomplete(true);
-                }
+              _infoCardLocation('Desde', _con.from ?? 'Lugar de recogida',
+                  () async {
+                _con.showGoogleAutocomplete(true);
+              }),
+              SizedBox(
+                height: 5,
               ),
-              SizedBox(height: 5,),
               Container(
-                width:  double.infinity,
+                width: double.infinity,
                 child: Divider(
                   color: Colors.grey,
                   height: 10,
                 ),
               ),
-              SizedBox(height: 5,),
-              _infoCardLocation(
-                  'Hasta',
-                  _con.to ?? 'Lugar de destino',
-                      () async {
-                    _con.showGoogleAutocomplete(false);
-                  }
+              SizedBox(
+                height: 5,
               ),
+              _infoCardLocation('Hasta', _con.to ?? 'Lugar de destino',
+                  () async {
+                _con.showGoogleAutocomplete(false);
+              }),
             ],
           ),
         ),
@@ -251,7 +251,7 @@ class _CLientMapPageState extends State<CLientMapPage> {
     );
   }
 
-  Widget _infoCardLocation(String title, String value, Function function){
+  Widget _infoCardLocation(String title, String value, Function function) {
     return GestureDetector(
       onTap: function,
       child: Column(
@@ -259,17 +259,12 @@ class _CLientMapPageState extends State<CLientMapPage> {
         children: [
           Text(
             title,
-            style: TextStyle(
-                color: Colors.grey,
-                fontSize: 10),
+            style: TextStyle(color: Colors.grey, fontSize: 10),
           ),
           Text(
             value,
             style: TextStyle(
-                color: Colors.black,
-                fontSize: 14,
-                fontWeight: FontWeight.bold
-            ),
+                color: Colors.black, fontSize: 14, fontWeight: FontWeight.bold),
             maxLines: 2,
           ),
         ],
@@ -277,7 +272,7 @@ class _CLientMapPageState extends State<CLientMapPage> {
     );
   }
 
-  void refresh(){
+  void refresh() {
     setState(() {});
   }
 }
--- lib/src/pages/client/travel_info/client_travel_info_page.dart
+++ lib/src/pages/client/travel_info/client_travel_info_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:give_structure/src/pages/client/travel_info/client_travel_info_controller.dart';
+import 'package:give_structure/src/widgets/button_app.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import 'package:flutter/scheduler.dart';
 
 class ClientTravelInfoPage extends StatefulWidget {
   const ClientTravelInfoPage({Key key}) : super(key: key);
@@ -13,10 +16,153 @@ class _ClientTravelInfoPageState extends State<ClientTravelInfoPage> {
   ClientTravelInfoController _con = new ClientTravelInfoController();
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+      _con.init(context, refresh);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: _con.key,
-      body: Stack(),
+      body: Stack(
+        children: [
+          Align(
+            child: _googleMapsWidget(),
+            alignment: Alignment.topCenter,
+          ),
+          Align(
+            child: _cardTravelInfo(),
+            alignment: Alignment.bottomCenter,
+          ),
+          Align(
+            child: _buttonBack(),
+            alignment: Alignment.topLeft,
+          ),
+          Align(
+            child: _cardKmInfo('0 Km'),
+            alignment: Alignment.topRight,
+          ),
+          Align(
+            child: _cardMinInfo('0 Min'),
+            alignment: Alignment.topRight,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardKmInfo(String km){
+    return SafeArea(
+        child: Container(
+          width: 100,
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          margin: EdgeInsets.only(right: 10, top: 10),
+          decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.all(Radius.circular(20))
+          ),
+          child: Text(km ?? '0 Km'),
+        )
+    );
+  }
+
+  Widget _cardMinInfo(String km){
+    return SafeArea(
+        child: Container(
+          width: 100,
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          margin: EdgeInsets.only(right: 10, top: 35),
+          decoration: BoxDecoration(
+              color: Colors.yellow,
+              borderRadius: BorderRadius.all(Radius.circular(20))
+          ),
+          child: Text(km ?? '0 Min'),
+        )
+    );
+  }
+
+  Widget _buttonBack(){
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.only(left: 10),
+        child: CircleAvatar(
+          radius: 20,
+          backgroundColor: Colors.white,
+          child: Icon(Icons.arrow_back, color: Colors.black),
+        ),
+      ),
+    );
+  }
+
+  Widget _cardTravelInfo(){
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.38,
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30))
+      ),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text(
+              'Desde',
+              style: TextStyle(
+                fontSize: 15
+              ),
+            ),
+            subtitle: Text(
+              'Cr falsa con calle falsa',
+              style: TextStyle(
+                fontSize: 13
+              ),
+            ),
+            leading: Icon(Icons.location_on),
+          ),
+          ListTile(
+            title: Text(
+              'Hasta',
+              style: TextStyle(
+                  fontSize: 15
+              ),
+            ),
+            subtitle: Text(
+              'Cr falsa con calle falsa',
+              style: TextStyle(
+                  fontSize: 13
+              ),
+            ),
+            leading: Icon(Icons.my_location),
+          ),
+          ListTile(
+            title: Text(
+              'Precio',
+              style: TextStyle(
+                  fontSize: 15
+              ),
+            ),
+            subtitle: Text(
+              '0.0 \$',
+              style: TextStyle(
+                  fontSize: 13
+              ),
+            ),
+            leading: Icon(Icons.attach_money),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 30),
+            child: ButtonApp(
+              onPressed: (){},
+              text: 'Confirmar',
+              textColor: Colors.black,
+              color: Colors.amber,
+            ),
+          )
+        ],
+      ),
     );
   }
 
@@ -27,13 +173,11 @@ class _ClientTravelInfoPageState extends State<ClientTravelInfoPage> {
       onMapCreated: _con.onMapCreated,
       myLocationEnabled: false,
       myLocationButtonEnabled: false,
-      markers: Set<Marker>.of(_con.markers.values),
-      onCameraMove: (position) {
-        _con.initialPosition = position;
-      },
-      onCameraIdle: () async {
-        // await _con.setLocationDraggableInfo();
-      },
+      markers: Set<Marker>.of(_con.markers.values)
     );
+  }
+
+  void refresh(){
+    setState(() {});
   }
 }
