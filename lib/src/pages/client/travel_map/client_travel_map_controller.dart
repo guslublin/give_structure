b2278bc1c61d46c7709b6dffe2ca04b9e40d140c
Gustavo Lublin <glublin@data.com.py>
2022-08-17 15:29:40 -0400

    80. Modal Bottom Sheet

--- lib/src/pages/client/travel_map/client_travel_map_controller.dart
+++ lib/src/pages/client/travel_map/client_travel_map_controller.dart
@@ -22,7 +22,6 @@ import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:give_structure/src/widgets/bottom_sheet_client_info.dart';
 
 class ClientTravelMapController {
-
   BuildContext context;
   Function refresh;
   GlobalKey<ScaffoldState> key = new GlobalKey<ScaffoldState>();
